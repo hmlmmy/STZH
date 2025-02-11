16e0908137bdd62088f49f9212255f857d8213ef
hmlmmy <feiyang.weng99@gmail.com>
2025-02-11 15:06:35 +0800

    add stzh initial frame

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ChatComponent />
+  </div>
+  <div class="chat-container">
+    <div v-for="(msg, index) in messages" :key="index">
+      <div :class="['message', msg.role]">
+        {{ msg.content }}
+      </div>
+    </div>
+    <el-input v-model="input" @keyup.enter="sendMessage">
+      <template #append>
+        <el-button @click="sendMessage">发送</el-button>
+      </template>
+    </el-input>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import ChatComponent from './components/ChatComponent.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const input = ref('');
+const messages = ref([]);
+
+const sendMessage = async () => {
+  if (!input.value.trim()) return;
+  
+  messages.value.push({ role: 'user', content: input.value });
+  const userInput = input.value;
+  input.value = '';
+  
+  try {
+    const response = await axios.post('/api/chat', {
+      question: userInput,
+      userId: 1 // 实际应使用登录用户ID
+    });
+    
+    messages.value.push({ 
+      role: 'assistant', 
+      content: response.data.answer 
+    });
+  } catch (error) {
+    console.error('请求失败:', error);
   }
-}
-</script>
+};
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
